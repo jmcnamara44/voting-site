4fe4a0f838540ec09e5d785d7f72876c998ff5f4
CJ Rhone and Jimmy McNamara <jimmy@>
2018-03-22 09:14:28 -0700

    Create link between button and scripts

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#votingForm").submit(function(event) {
+
+    event.preventDefault();
+
+    $(".ofAge").show();
+});
+    // var ageInput = $("input#age").val();
+
+  //   if (ageInput >= 18) {
+  //     $(".ofAge").show();
+  //
+  //   } else {
+  //     $(".underAge").show();
+  //
+  //   }
+  //
+  });
